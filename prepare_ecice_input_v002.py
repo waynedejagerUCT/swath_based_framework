4b6491e265e889efd7eb58acc4c527c743ed0eee
Wayne de Jager <waynedj17@gmail.com>
2026-01-20 15:29:23 +0200

    Improved readability of scipts.

--- prepare_ecice_input_v002.py
+++ prepare_ecice_input_v002.py
@@ -17,93 +17,79 @@ if __name__ == '__main__':
     list_of_files = ['GW1AM2_201907261709_107A_L1SGRTBR_2220220.h5','GW1AM2_201907270213_203D_L1SGRTBR_2220220.h5']
 
     for file in list_of_files:
-        print('Filename: ' + file)
+        print('Preparing swath filename: ' + file)
+        with h5py.File(dir_amsr2 + file, mode='r') as H:
+            # fetch brightness temperatures from low frequency channels
+            resample_res = 'res23'
+            tb_19_h  = H[f'Brightness Temperature ({resample_res},18.7GHz,H)'][:]/100
+            tb_19_v  = H[f'Brightness Temperature ({resample_res},18.7GHz,V)'][:]/100
+            tb_22_h  = H[f'Brightness Temperature ({resample_res},23.8GHz,H)'][:]/100
+            tb_22_v  = H[f'Brightness Temperature ({resample_res},23.8GHz,V)'][:]/100
+            tb_37_h  = H[f'Brightness Temperature ({resample_res},36.5GHz,H)'][:]/100
+            tb_37_v  = H[f'Brightness Temperature ({resample_res},36.5GHz,V)'][:]/100
+            # generate latlon grid compatible with low frequency channels by modifying available 89GHz grid
+            lat_a = H['Latitude of Observation Point for 89A'][:]
+            lat   = lat_a[:,1::2]
+            lon_a = H['Longitude of Observation Point for 89A'][:]
+            lon   = lon_a[:,1::2]
 
-        H        = h5py.File(dir_amsr2+file, mode='r')
-        # fetch brightness temperatures from low frequency channels
-        resample_res = 'res23'
-        tb_19_h  = H[f'Brightness Temperature ({resample_res},18.7GHz,H)'][:]/100
-        tb_19_v  = H[f'Brightness Temperature ({resample_res},18.7GHz,V)'][:]/100
-        tb_22_h  = H[f'Brightness Temperature ({resample_res},23.8GHz,H)'][:]/100
-        tb_22_v  = H[f'Brightness Temperature ({resample_res},23.8GHz,V)'][:]/100
-        tb_37_h  = H[f'Brightness Temperature ({resample_res},36.5GHz,H)'][:]/100
-        tb_37_v  = H[f'Brightness Temperature ({resample_res},36.5GHz,V)'][:]/100
-        # generate latlon grid compatible with low frequency channels by modifying available 89GHz grid
-        lat_a = H['Latitude of Observation Point for 89A'][:]
-        lat   = lat_a[:,1::2]
-        lon_a = H['Longitude of Observation Point for 89A'][:]
-        lon   = lon_a[:,1::2]
+            # generate indexing for ECICE
+            grid_shape   = tb_19_h.shape
+            grid_size    = grid_shape[0]*grid_shape[1]
+            lines, cells = np.meshgrid(range(grid_shape[1]), range(grid_shape[0]))
+            # generate empty backscattering array (backscattering not used for swath ECICE but algorithm still expects input)
+            bks          = np.zeros(grid_size)
+            # compute auxilary variables
+            pr19         = (tb_19_v - tb_19_h)/(tb_19_v + tb_19_h)
+            pr22         = (tb_22_v - tb_22_h)/(tb_22_v + tb_22_h)
+            pr37         = (tb_37_v - tb_37_h)/(tb_37_v + tb_37_h)
+            gr37v19v     = (tb_37_v - tb_19_v)/(tb_37_v + tb_19_v)
+            gr22v19v     = (tb_22_v - tb_19_v)/(tb_22_v + tb_19_v)
+            # generate land mask
+            land         = H['Land_Ocean Flag 6 to 36'][:][0]
+            #close h5 file
+            H.close()
+            # Flatten lat once (curvilinear is fine; we just need elementwise comparison)
+            lat1         = lat.reshape(-1)
+            # Mask: True where we want to blank out (lat > -55)
+            mask         = lat1 > -55
+            # Helper to blank out values where mask is True
+            def nan_where(x):
+                x1 = x.reshape(-1).astype(float)  # ensure float so NaN is allowed
+                x1[mask] = np.nan
+                return x1
 
-        # generate indexing for ECICE
-        grid_shape   = tb_19_h.shape
-        grid_size    = grid_shape[0]*grid_shape[1]
-        lines, cells = np.meshgrid(range(grid_shape[1]), range(grid_shape[0]))
-        # generate empty backscattering array (backscattering not used for swath ECICE but algorithm still expects input)
-        bks          = np.zeros(grid_size)
-        # compute auxilary variables
-        pr19         = (tb_19_v - tb_19_h)/(tb_19_v + tb_19_h)
-        pr22         = (tb_22_v - tb_22_h)/(tb_22_v + tb_22_h)
-        pr37         = (tb_37_v - tb_37_h)/(tb_37_v + tb_37_h)
-        gr37v19v     = (tb_37_v - tb_19_v)/(tb_37_v + tb_19_v)
-        gr22v19v     = (tb_22_v - tb_19_v)/(tb_22_v + tb_19_v)
-        # generate land mask
-        land         = H['Land_Ocean Flag 6 to 36'][:][0]
-        #close h5 file
-        H.close()
-        # Flatten lat once (curvilinear is fine; we just need elementwise comparison)
-        lat1         = lat.reshape(-1)
-        # Mask: True where we want to blank out (lat > -55)
-        mask         = lat1 > -55
-        # Helper to blank out values where mask is True
-        def nan_where(x):
-            x1 = x.reshape(-1).astype(float)  # ensure float so NaN is allowed
-            x1[mask] = np.nan
-            return x1
+            out = np.c_[
+                lines.reshape(-1),          #0
+                cells.reshape(-1),          #1
+                lat1,                       #2
+                lon.reshape(-1),            #3
+                nan_where(bks),             #4
+                nan_where(tb_19_h),         #5
+                nan_where(tb_19_v),         #6
+                nan_where(tb_22_h),         #7
+                nan_where(tb_22_v),         #8
+                nan_where(tb_37_h),         #9
+                nan_where(tb_37_v),         #10
+                nan_where(pr19),            #11
+                nan_where(pr22),            #12
+                nan_where(pr37),            #13
+                nan_where(gr37v19v),        #14
+                nan_where(gr22v19v),        #15
+                nan_where(land),            #16
+            ]
+            
+            headers    = ['ORLINE','ORCELL','LATITD','LONGTD','BKAS','BRT19H','BRT19V','BRT22H','BRT22V','BRT37H','BRT37V','PR19','PR22','PR37','GD37v19v','GR22v19v','LAND']
+            formatting = ['%i'    ,'%i'    ,'%.2f'  ,'%.2f'  ,'%.2f','%.2f'  ,'%.2f'  ,'%.2f'  ,'%.2f'  ,'%.2f'  ,'%.2f'  ,'%.6f','%.6f','%.6f','%.6f'    ,'%.6f'    ,'%.2f']
+            fname_out  = dir_output + file[0:-3] + '-intermediate-v001.txt'
 
-        out = np.c_[
-            lines.reshape(-1),          #0
-            cells.reshape(-1),          #1
-            lat1,                       #2  (your comment says #3, but this is the 3rd column)
-            lon.reshape(-1),            #3
-            bks.reshape(-1),            #4
-            nan_where(tb_19_h),         #5
-            nan_where(tb_19_v),         #6
-            nan_where(tb_22_h),         #7
-            nan_where(tb_22_v),         #8
-            nan_where(tb_37_h),         #9
-            nan_where(tb_37_v),         #10
-            nan_where(pr19),            #11
-            nan_where(pr22),            #12
-            nan_where(pr37),            #13
-            nan_where(gr37v19v),        #14
-            nan_where(gr22v19v),        #15
-            land.reshape(-1)            #16
-        ]
-        
-        # save data to .txt file
-        fname_out = dir_output + file[0:-3] + '-intermediate-v001.txt'
-        np.savetxt(fname_out, out, delimiter='\t', fmt=['%i','%i','%.2f','%.2f','%.2f','%.2f','%.2f','%.2f','%.2f','%.2f','%.2f','%.6f','%.6f','%.6f','%.6f','%.6f','%.2f'])
-        # generate an empty temporary .txt file
-        fname_tmp = dir_output + file[0:-3] + '-intermediate-v001-tmp.txt'
-        # open data and temporary .txt files
-        file_orig = open(fname_out,'r')
-        file_tmp  = open(fname_tmp,'w')
-        # define column headers
-        headers = ['ORLINE','ORCELL','LATITD','LONGTD','BKAS','BRT19H','BRT19V','BRT22H','BRT22V','BRT37H','BRT37V','PR19','PR22','PR37','GD37v19v','GR22v19v','LAND']
-        
-        # write column headers as first row in temporary .txt file
-        file_tmp.write('\t'.join(headers)+'\n')
-        # iterate through each row in data .txt file and append row to temporary .txt file
-        for line in file_orig.readlines():
-            file_tmp.write(line)
-        # close both data and tempory .txt files
-        file_orig.close()
-        file_tmp.close()
-        # remove data .txt file and rename temporary .txt file to original file name
-        os.remove(fname_out)
-        os.rename(fname_tmp, fname_out)
-
-
+            np.savetxt(
+                fname_out,
+                out,
+                delimiter='\t',
+                fmt=formatting,
+                header='\t'.join(headers),
+                comments='')
 
     timeit_t1 = timeit.default_timer()
 
--- prepare_ecice_output_v002.py
+++ prepare_ecice_output_v002.py
@@ -44,12 +44,12 @@ def get_grids(grid_res):
 
 def swath_remapping(in_lat, in_lon, in_data, out_grid, sample_radius):
 
-    sample_radius = sample_radius*1000 #units km to meters
+    roi_m = sample_radius * 1000 #units km to meters
     # input grid: input swath data
     in_grid  = pr.geometry.SwathDefinition(lons=pr.utils.wrap_longitudes(in_lon), lats=in_lat)
     # remap input_grid onto output_grid
-    grdata   = pr.kd_tree.resample_nearest(in_grid, in_data, out_grid, radius_of_influence=sample_radius, fill_value=np.nan)
-
+    grdata   = pr.kd_tree.resample_nearest(in_grid, in_data, out_grid, radius_of_influence=roi_m, fill_value=np.nan)
+    
     return grdata
 
 
@@ -58,8 +58,7 @@ if __name__ == '__main__':
     timeit_t0 = timeit.default_timer()
 
     #parameters and directories
-    amsre_conversion = True
-    grid_res         = 12.5
+    grid_res         = 25
     sample_radius    = grid_res*1.5
 
     dir_input        = '/home/waynedj/Data/intermediate/ecice/'
@@ -76,155 +75,125 @@ if __name__ == '__main__':
     xc  = xc[np.newaxis,:,:]
     yc  = yc[np.newaxis,:,:]
 
-    #               0     1      2     3      4    5      6       7      8      9     10    11   12   13     14       15     16    17   18   19    20     21       22     23      24      25       26      27      28       29      30
-    #headers = [ORLINE,ORCELL,LATITD,LONGTD,BKAS,BRT19H,BRT19V,BRT22H,BRT22V,BRT37H,BRT37V,PR19,PR22,PR37,GD37v19v,GR22v19v,LAND,C_OW,C_FYI,C_YI,C_MYI,C_TOTAL,CONF_OW,CONF_I1,CONF_I2,CONF_I3,CONF_VEC,ITERAVG,OW_FILT,GR_FILT,CLOUD_FILT]
-    
-    usecols = [
-    0, 1,   # ORLINE, ORCELL (needed to rebuild 2D swath)
-    2, 3,   # LATITD, LONGTD
-    16,     # LAND
-    17, 18, 19, 20, 21,  # C_OW, C_FYI, C_YI, C_MYI, C_TOTAL
-    22, 23, 24, 25,      # CONF_OW, CONF_I1, CONF_I2, CONF_I3
-    26                   # CONF_VEC
-    ]
+    COL = dict(ORLINE=0,ORCELL=1,LAT=2,LON=3,BKAS=4,BRT19H=5,BRT19V=6,BRT22H=7,BRT22V=8,
+               BRT37H=9,BRT37V=10,PR19=11,PR22=12,PR37=13,GD37v19v=14,GR22v19v=15,LAND=16,
+               C_OW=17,C_FYI=18,C_YI=19,C_MYI=20,C_TOT=21,CONF_OW=22,CONF_I1=23,
+               CONF_I2=24,CONF_I3=25,CONF_VEC=2)
     
     for file in list_of_files:
-        
-        if '-ECICE' in file:
-            print('Converting ' + file + ' from txt to nc')
-            # fetch data from intermediate .txt file
-            data = np.genfromtxt(dir_input + file, skip_header=1, usecols=usecols)
-            # Unpack 1D columns
-            ORLINE   = data[:, 0].astype(int)
-            ORCELL   = data[:, 1].astype(int)
-            LATITD_1 = data[:, 2].astype(float)
-            LONGTD_1 = data[:, 3].astype(float)
-            LAND_1   = data[:, 4].astype(float)
-            C_OW_1   = data[:, 5].astype(float)
-            C_FYI_1  = data[:, 6].astype(float)
-            C_YI_1   = data[:, 7].astype(float)
-            C_MYI_1  = data[:, 8].astype(float)
-            C_TOT_1  = data[:, 9].astype(float)
-            CONF_OW_1  = data[:, 10].astype(float)
-            CONF_I1_1  = data[:, 11].astype(float)
-            CONF_I2_1  = data[:, 12].astype(float)
-            CONF_I3_1  = data[:, 13].astype(float)
-            CONF_VEC_1 = data[:, 14].astype(float)
-
-            # Rebuild native swath 2D arrays using ORLINE/ORCELL (order independent)
-            swath_lat  = scatter_to_grid(ORLINE, ORCELL, LATITD_1, fill=np.nan, dtype=float)
-            swath_lon  = scatter_to_grid(ORLINE, ORCELL, LONGTD_1, fill=np.nan, dtype=float)
-            LAND   = scatter_to_grid(ORLINE, ORCELL, LAND_1, fill=np.nan, dtype=float)
-            OW     = scatter_to_grid(ORLINE, ORCELL, C_OW_1, fill=np.nan, dtype=float)
-            FYI    = scatter_to_grid(ORLINE, ORCELL, C_FYI_1, fill=np.nan, dtype=float)
-            YI     = scatter_to_grid(ORLINE, ORCELL, C_YI_1, fill=np.nan, dtype=float)
-            MYI    = scatter_to_grid(ORLINE, ORCELL, C_MYI_1, fill=np.nan, dtype=float)
-            TI     = scatter_to_grid(ORLINE, ORCELL, C_TOT_1, fill=np.nan, dtype=float)
-            CONF_OW  = scatter_to_grid(ORLINE, ORCELL, CONF_OW_1, fill=np.nan, dtype=float)
-            CONF_I1  = scatter_to_grid(ORLINE, ORCELL, CONF_I1_1, fill=np.nan, dtype=float)
-            CONF_I2  = scatter_to_grid(ORLINE, ORCELL, CONF_I2_1, fill=np.nan, dtype=float)
-            CONF_I3  = scatter_to_grid(ORLINE, ORCELL, CONF_I3_1, fill=np.nan, dtype=float)
-            CONF_VEC = scatter_to_grid(ORLINE, ORCELL, CONF_VEC_1, fill=np.nan, dtype=float)
-
-            # Optional: If you used a “check_nan” channel before, you can define one here.
-            # Example: treat missing lat/lon as missing retrieval
-            ind0 = np.isnan(swath_lat) | np.isnan(swath_lon)
-
-            for arr in [OW, FYI, YI, MYI, TI, CONF_I1, CONF_I2, CONF_I3, CONF_OW, CONF_VEC]:
-                arr[ind0] = np.nan
-
-            # Handle ECICE open-water filter (TI==102)
-            ind1 = np.where(TI == 102)
-            OW[ind1] = 100
-            FYI[ind1] = 0
-            YI[ind1] = 0
-            MYI[ind1] = 0
-            TI[ind1] = 0
-            CONF_I1[ind1] = np.nan
-            CONF_I2[ind1] = np.nan
-            CONF_I3[ind1] = np.nan
-            CONF_OW[ind1] = np.nan
-            CONF_VEC[ind1] = np.nan
-
-            # --------------------------------------------------------------------
-            # Resample swath -> EASE2 grid using your existing swath_remapping()
-            # --------------------------------------------------------------------
-            OW_g       = swath_remapping(swath_lat, swath_lon, OW,       out_grid, sample_radius)
-            FYI_g      = swath_remapping(swath_lat, swath_lon, FYI,      out_grid, sample_radius)
-            YI_g       = swath_remapping(swath_lat, swath_lon, YI,       out_grid, sample_radius)
-            MYI_g      = swath_remapping(swath_lat, swath_lon, MYI,      out_grid, sample_radius)
-            TI_g       = swath_remapping(swath_lat, swath_lon, TI,       out_grid, sample_radius)
-            CONF_I1_g  = swath_remapping(swath_lat, swath_lon, CONF_I1,  out_grid, sample_radius)
-            CONF_I2_g  = swath_remapping(swath_lat, swath_lon, CONF_I2,  out_grid, sample_radius)
-            CONF_I3_g  = swath_remapping(swath_lat, swath_lon, CONF_I3,  out_grid, sample_radius)
-            CONF_OW_g  = swath_remapping(swath_lat, swath_lon, CONF_OW,  out_grid, sample_radius)
-            CONF_VEC_g = swath_remapping(swath_lat, swath_lon, CONF_VEC, out_grid, sample_radius)
-
-            # LAND is categorical; nearest-neighbour is OK, but you may also keep LAND from your mask.
-            LAND_g     = swath_remapping(swath_lat, swath_lon, LAND,     out_grid, sample_radius)
-                        
-            # Add time dimension
-            OW_g       = OW_g[np.newaxis, :, :]
-            FYI_g      = FYI_g[np.newaxis, :, :]
-            YI_g       = YI_g[np.newaxis, :, :]
-            MYI_g      = MYI_g[np.newaxis, :, :]
-            TI_g       = TI_g[np.newaxis, :, :]
-            CONF_I1_g  = CONF_I1_g[np.newaxis, :, :]
-            CONF_I2_g  = CONF_I2_g[np.newaxis, :, :]
-            CONF_I3_g  = CONF_I3_g[np.newaxis, :, :]
-            CONF_OW_g  = CONF_OW_g[np.newaxis, :, :]
-            CONF_VEC_g = CONF_VEC_g[np.newaxis, :, :]
-            LAND_g     = LAND_g[np.newaxis, :, :]
-
-            # Parse time from filename (keep your approach)
-            year   = int(file[7:11])
-            month  = int(file[11:13])
-            day    = int(file[13:15])
-            hour   = int(file[15:17])
-            minute = int(file[17:19])
-            second = 0
-            t = np.array([datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second)])
-
-            # Create Dataset
-            ds = xr.Dataset(
-                data_vars=dict(
-                    xc=(['time','x','y'], xc),
-                    yc=(['time','x','y'], yc),
-                    OW=(['time','x','y'], OW_g),
-                    YI=(['time','x','y'], YI_g),
-                    FYI=(['time','x','y'], FYI_g),
-                    MYI=(['time','x','y'], MYI_g),
-                    TI=(['time','x','y'], TI_g),
-                    CONF_I1=(['time','x','y'], CONF_I1_g),
-                    CONF_I2=(['time','x','y'], CONF_I2_g),
-                    CONF_I3=(['time','x','y'], CONF_I3_g),
-                    CONF_OW=(['time','x','y'], CONF_OW_g),
-                    CONF_VEC=(['time','x','y'], CONF_VEC_g),
-                    LAND=(['time','x','y'], LAND_g),
-                ),
-                coords=dict(
-                    longitude=(['x','y'], lons),
-                    latitude=(['x','y'], lats),
-                    time=t
-                ),
-                attrs=dict(
-                    Title='ECICE applied to AMSR-2 swaths',
-                    Description='ECICE outputs reconstructed on native ORLINE/ORCELL swath grid, then resampled to EASE2.',
-                    Hemisphere='Southern Hemisphere',
-                    Grid_resolution=str(grid_res) + ' km',
-                    Grid_projection='EASE2',
-                    Author='Wayne de Jager, Department of Oceanography, University of Cape Town, South Africa',
-                    DistVersion='v0.0.2'
-                )
-            )
-
-            # Save dataset to netcdf
-            ds.to_netcdf(dir_output + file[0:41] + file[59:65] + '.nc', format='NETCDF4')
-
-            
-
-        else:
+        if '-ECICE' not in file:
             continue
+        
+        print('Mapping ' + file + ' from txt to EASE2 grid in netcdf format.')
+        # fetch data from intermediate .txt file
+        data       = np.genfromtxt(os.path.join(dir_input, file), skip_header=1)
+        # Unpack 1D columns
+        ORLINE     = data[:, COL["ORLINE"]].astype(int)
+        ORCELL     = data[:, COL["ORCELL"]].astype(int)
+        LATITD_1   = data[:, COL["LAT"]].astype(float)
+        LONGTD_1   = data[:, COL["LON"]].astype(float)
+        LAND_1     = data[:, COL["LAND"]].astype(float)
+        C_OW_1     = data[:, COL["C_OW"]].astype(float)
+        C_FYI_1    = data[:, COL["C_FYI"]].astype(float)
+        C_YI_1     = data[:, COL["C_YI"]].astype(float)
+        C_MYI_1    = data[:, COL["C_MYI"]].astype(float)
+        C_TOT_1    = data[:, COL["C_TOT"]].astype(float)
+        CONF_OW_1  = data[:, COL["CONF_OW"]].astype(float)
+        CONF_I1_1  = data[:, COL["CONF_I1"]].astype(float)
+        CONF_I2_1  = data[:, COL["CONF_I2"]].astype(float)
+        CONF_I3_1  = data[:, COL["CONF_I3"]].astype(float)
+        CONF_VEC_1 = data[:, COL["CONF_VEC"]].astype(float)
+        # Rebuild native swath 2D arrays using ORLINE/ORCELL (order independent)
+        swath_lat  = scatter_to_grid(ORLINE, ORCELL, LATITD_1, fill=np.nan, dtype=float)
+        swath_lon  = scatter_to_grid(ORLINE, ORCELL, LONGTD_1, fill=np.nan, dtype=float)
+        LAND       = scatter_to_grid(ORLINE, ORCELL, LAND_1, fill=np.nan, dtype=float)
+        OW         = scatter_to_grid(ORLINE, ORCELL, C_OW_1, fill=np.nan, dtype=float)
+        FYI        = scatter_to_grid(ORLINE, ORCELL, C_FYI_1, fill=np.nan, dtype=float)
+        YI         = scatter_to_grid(ORLINE, ORCELL, C_YI_1, fill=np.nan, dtype=float)
+        MYI        = scatter_to_grid(ORLINE, ORCELL, C_MYI_1, fill=np.nan, dtype=float)
+        TI         = scatter_to_grid(ORLINE, ORCELL, C_TOT_1, fill=np.nan, dtype=float)
+        CONF_OW    = scatter_to_grid(ORLINE, ORCELL, CONF_OW_1, fill=np.nan, dtype=float)
+        CONF_I1    = scatter_to_grid(ORLINE, ORCELL, CONF_I1_1, fill=np.nan, dtype=float)
+        CONF_I2    = scatter_to_grid(ORLINE, ORCELL, CONF_I2_1, fill=np.nan, dtype=float)
+        CONF_I3    = scatter_to_grid(ORLINE, ORCELL, CONF_I3_1, fill=np.nan, dtype=float)
+        CONF_VEC   = scatter_to_grid(ORLINE, ORCELL, CONF_VEC_1, fill=np.nan, dtype=float)
+
+        # Handle ECICE open-water filter (TI==102)
+        ind1           = np.where(TI == 102)
+        OW[ind1]       = 100
+        FYI[ind1]      = 0
+        YI[ind1]       = 0
+        MYI[ind1]      = 0
+        TI[ind1]       = 0
+        CONF_I1[ind1]  = np.nan
+        CONF_I2[ind1]  = np.nan
+        CONF_I3[ind1]  = np.nan
+        CONF_OW[ind1]  = np.nan
+        CONF_VEC[ind1] = np.nan
+        
+        # Resample swath -> EASE2 grid
+        OW_g       = swath_remapping(swath_lat, swath_lon, OW,       out_grid, sample_radius)
+        FYI_g      = swath_remapping(swath_lat, swath_lon, FYI,      out_grid, sample_radius)
+        YI_g       = swath_remapping(swath_lat, swath_lon, YI,       out_grid, sample_radius)
+        MYI_g      = swath_remapping(swath_lat, swath_lon, MYI,      out_grid, sample_radius)
+        TI_g       = swath_remapping(swath_lat, swath_lon, TI,       out_grid, sample_radius)
+        CONF_I1_g  = swath_remapping(swath_lat, swath_lon, CONF_I1,  out_grid, sample_radius)
+        CONF_I2_g  = swath_remapping(swath_lat, swath_lon, CONF_I2,  out_grid, sample_radius)
+        CONF_I3_g  = swath_remapping(swath_lat, swath_lon, CONF_I3,  out_grid, sample_radius)
+        CONF_OW_g  = swath_remapping(swath_lat, swath_lon, CONF_OW,  out_grid, sample_radius)
+        CONF_VEC_g = swath_remapping(swath_lat, swath_lon, CONF_VEC, out_grid, sample_radius)
+
+        # LAND is categorical; nearest-neighbour is OK, but you may also keep LAND from your mask.
+        LAND_g     = swath_remapping(swath_lat, swath_lon, LAND, out_grid, sample_radius)
+                    
+        # Add time dimension
+        OW_g, FYI_g, YI_g, MYI_g, TI_g, CONF_I1_g, CONF_I2_g, CONF_I3_g, CONF_OW_g, CONF_VEC_g, LAND_g = [a[np.newaxis, :, :] for a in (OW_g, FYI_g, YI_g, MYI_g, TI_g, CONF_I1_g, CONF_I2_g, CONF_I3_g, CONF_OW_g, CONF_VEC_g, LAND_g)]
+
+        # Parse time from filename format: GW1AM2_YYYYMMDDhhmm_...
+        year   = int(file[7:11])
+        month  = int(file[11:13])
+        day    = int(file[13:15])
+        hour   = int(file[15:17])
+        minute = int(file[17:19])
+        second = 0
+        t = np.array([datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second)])
+
+        # Create Dataset
+        ds = xr.Dataset(
+            data_vars=dict(
+                xc=(['time','x','y'], xc),
+                yc=(['time','x','y'], yc),
+                OW=(['time','x','y'], OW_g),
+                YI=(['time','x','y'], YI_g),
+                FYI=(['time','x','y'], FYI_g),
+                MYI=(['time','x','y'], MYI_g),
+                TI=(['time','x','y'], TI_g),
+                CONF_I1=(['time','x','y'], CONF_I1_g),
+                CONF_I2=(['time','x','y'], CONF_I2_g),
+                CONF_I3=(['time','x','y'], CONF_I3_g),
+                CONF_OW=(['time','x','y'], CONF_OW_g),
+                CONF_VEC=(['time','x','y'], CONF_VEC_g),
+                LAND=(['time','x','y'], LAND_g),
+            ),
+            coords=dict(
+                longitude=(['x','y'], lons),
+                latitude=(['x','y'], lats),
+                time=t
+            ),
+            attrs=dict(
+                Title='ECICE applied to AMSR-2 swaths',
+                Description='ECICE outputs reconstructed on native ORLINE/ORCELL swath grid, then resampled to EASE2.',
+                Hemisphere='Southern Hemisphere',
+                Grid_resolution=str(grid_res) + ' km',
+                Grid_projection='EASE2',
+                Author='Wayne de Jager, Department of Oceanography, University of Cape Town, South Africa',
+                DistVersion='v0.0.2'
+            )
+        )
+
+        # Save dataset to netcdf
+        ds.to_netcdf(dir_output + file[0:41] + file[59:65] + '.nc', format='NETCDF4')
+
 
 
     timeit_t1 = timeit.default_timer()
